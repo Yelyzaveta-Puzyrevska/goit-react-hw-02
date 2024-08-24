942be32d64af3df845bfac4194545e7b98aec4b5
Yelyzaveta Puzyrevska <169177255+Yelyzaveta-Puzyrevska@users.noreply.github.com>
2024-08-24 23:12:53 +0300

    feedback counter

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,17 +1,10 @@
-const Options = ({ options, onFeedback, totalFeedback, onReset }) => {
+const Options = ({ onFeedback, totalFeedback, onReset }) => {
   return (
     <div>
-      <ul>
-        <li>Good: {options.good}</li>
-        <li>Neutral: {options.neutral}</li>
-        <li>Bad: {options.bad}</li>
-      </ul>
-      <div>
-        <button onClick={() => onFeedback("good")}>Good</button>
-        <button onClick={() => onFeedback("neutral")}>Neutral</button>
-        <button onClick={() => onFeedback("bad")}>Bad</button>
-        {totalFeedback > 0 && <button onClick={onReset}>Reset</button>}
-      </div>
+      <button onClick={() => onFeedback("good")}>Good</button>
+      <button onClick={() => onFeedback("neutral")}>Neutral</button>
+      <button onClick={() => onFeedback("bad")}>Bad</button>
+      {totalFeedback > 0 && <button onClick={onReset}>Reset</button>}
     </div>
   );
 };
